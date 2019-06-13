2813b4e0b210b6a62c98c632b06cf6aed49a9e6e
MattVinall <matt.vinall7@gmail.com>
2019-06-13 15:26:38 -0400

    added component structure and adjusted the paths

---
+++ src/components/SearchEmojis.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class SearchEmojis extends React.Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<div className="searchBar">
+				<label for="search" />
+				<input type="search" labelFor="search" placeholder="search your favourite emojis" id="search" />
+				<input type="submit" />
+			</div>
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DisplayEmojis from './components/DisplayEmojis';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<DisplayEmojis />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
